6113e739bfb3530612d8683854e941749b7e0e36
Juliano Costa <julianohcosta@gmail.com>
2021-09-14 19:00:00 -0400

    blog project

---
+++ frontend/blog/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { ContatoComponent } from './contato/contato.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'feed',
+    component: FeedComponent,
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
